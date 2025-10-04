321778e14cdeb7ed45cddfa6b2f9ec82779ce640
Hawkay002 <119169431+Hawkay002@users.noreply.github.com>
2025-10-05 01:19:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
 TOKEN = os.environ.get("TOKEN")
-IMAGE_FILE = "Birthday Wish.png"  # your uploaded image file
+IMAGE_FILE = "Birthday Wish.png"  # your uploaded picture file
 
 # /start command
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
